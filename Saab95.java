6644f21e1e518a3a56e61812aa714f049acd0e06
Gusphida <97883810+Gusphida@users.noreply.github.com>
2022-01-24 20:05:11 +0100

    Add files via upload

--- Saab95.java
+++ Saab95.java
@@ -1,79 +1,47 @@
 import java.awt.*;
+/**
+ * The Saab95 class introduces a car with specific values
+ **/
 
-public class Saab95{
+public class Saab95 extends ACar {
+    private boolean turboOn;
 
-    public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    
-    public Saab95(){
+    /**
+     * Default values for a Saab95 car
+     */
+    public Saab95() {
         nrDoors = 2;
         color = Color.red;
         enginePower = 125;
-	    turboOn = false;
+        turboOn = false;
         modelName = "Saab95";
         stopEngine();
     }
-    
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
+
+    /**
+     * Sets the turbo on
+     */
+    public void setTurboOn() {
+        //Sets turboOn to true
+        turboOn = true;
     }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
+    /**
+     * Turns the turbo off
+     */
+    public void setTurboOff() {
+        //Sets turboOn to false
+        turboOn = false;
     }
 
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-
-    public void setTurboOn(){
-	    turboOn = true;
-    }
-
-    public void setTurboOff(){
-	    turboOn = false;
-    }
-    
-    public double speedFactor(){
+    /**
+     * Calculates the cars speed factor
+     * @return the cars speed factor
+     */
+    public double speedFactor() {
+        //Calculates the speedFactor
         double turbo = 1;
-        if(turboOn) turbo = 1.3;
+        if (turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
-    }
-
-    public void incrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-    }
-    
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(amount);
-    }
-
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(amount);
     }
 }
--- Volvo240.java
+++ Volvo240.java
@@ -1,14 +1,14 @@
 import java.awt.*;
+/**
+ * The Volvo240 class introduces a car with specific values
+ **/
 
-public class Volvo240{
+public class Volvo240 extends ACar {
+    private final static double trimFactor = 1.25;
 
-    public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    
+    /**
+     * Default values for a Volvo240 car
+     */
     public Volvo240(){
         nrDoors = 4;
         color = Color.black;
@@ -16,53 +16,13 @@ public class Volvo240{
         modelName = "Volvo240";
         stopEngine();
     }
-    
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-    
+    /**
+     * Calculates the car's speed factor
+     * @return the car's speed factor
+     */
     public double speedFactor(){
+        //Calculates the speedFactor
         return enginePower * 0.01 * trimFactor;
-    }
-
-    public void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
-    }
-
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(amount);
-    }
-
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(amount);
     }
 }
